cad9586ae4f52c410d6fdefb04e38c30288bd329
Martin Gerlach <mgerlach@wikimedia.org>
2023-09-15 15:49:00 +0200

    adding endpoint for out predictions

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ CORS(app)
 
 print("Try: http://127.0.0.1:5000/api/v1/in?lang=en&title=Alcohol_Act_(Switzerland)&ltrans=de|fr")
 print("Try: http://127.0.0.1:5000/api/v1/in?lang=en&title=Degersee&ltrans=de|fr")
+print("Try: http://127.0.0.1:5000/api/v1/out?lang=en&title=Tiwanaku")
+print("Try: http://127.0.0.1:5000/api/v1/out?lang=en&title=Tiwanaku&ltrans=ca|es|fr")
+
 
 @app.route('/')
 def index():
@@ -55,6 +58,7 @@ def get_recommendations():
     df = pd.DataFrame(list_links)
     if len(df)>0:
         df_formatted = df.groupby(by="source", as_index=False).agg(list)
+        # sort recs by number of wikis it already exists in (n)
         df_formatted["n"] = df_formatted.apply(lambda x: len(x["source_translate"]),axis=1)
         df_formatted=df_formatted.sort_values(by="n",ascending=False)
 
@@ -82,6 +86,64 @@ def get_recommendations():
     return jsonify(out_json)
     # else:
     #     return jsonify({'Error':"No results"})
+
+@app.route('/api/v1/out', methods=['GET'])
+def get_recommendations_out():
+    n_wikis_min = 2
+    wiki_lang = set_lang()
+    wiki_db = wiki_lang+"wiki"
+    page_title = set_title()
+    langs_translate = request.args.get('ltrans')
+    if langs_translate!=None and isinstance(langs_translate,str):
+        langs_translate = langs_translate.split("|")
+    dict_result =  utils.link_translate_outlink(page_title, wiki_db, langs_translate=langs_translate)
+    list_links = dict_result["outlinks_recs"]
+    page_title_outlinks = dict_result["outlinks_exist"]
+
+    ## aggregating across langs
+    results = []
+    df = pd.DataFrame(list_links)
+    if len(df)>0:
+        df_formatted = df.groupby(by="target", as_index=False).agg(list)
+        df_formatted["n"] = df_formatted.apply(lambda x: len(x["target_translate"]),axis=1)
+        # sort recs by number of wikis in which link already exists
+        df_formatted=df_formatted.sort_values(by="n",ascending=False)
+
+        df_formatted["links"] = df_formatted.apply(lambda x: [ {
+            "lang_translate":x["lang_translate"][i],
+            "link_translate":(x["source_translate"][i],x["target_translate"][i])
+        } for i in range(len(x["source"]))] ,axis=1)
+
+        # keep only links that exist in at n_wiki_min other wikis in order to reduce the potential list
+        df_formatted = df_formatted[df_formatted["n"]>=n_wikis_min]
+
+        # get the kin of all recs
+        df_formatted["kin"] = df_formatted.apply(lambda x: len(utils.get_page_inlinks(x["target"], wiki_db, do_continue=False)),axis=1)
+
+
+        # sort first by kin, second by n_wikis
+        df_formatted = df_formatted.sort_values(by=["kin","n"], ascending=[True,False])
+
+
+
+        # keep only top-10 suggestions
+        for i in range(min([10,len(df_formatted)])):
+            dict_out = {
+                "target":df_formatted["target"].iloc[i],
+                "n_wikis": int(df_formatted["n"].iloc[i]),
+                "kin": int(df_formatted["kin"].iloc[i]),
+                "links": df_formatted["links"].iloc[i],
+            }
+            results+=[dict_out]
+
+    out_json = {
+        "page_title": page_title,
+        "lang": wiki_lang,
+        "article": "https://{0}.wikipedia.org/wiki/{1}".format(wiki_lang,page_title),
+        "n_outlinks": len(page_title_outlinks),
+        "results": results
+    }
+    return jsonify(out_json)
 
 def set_lang():
     if 'lang' in request.args:
--- utils.py
+++ utils.py
@@ -35,11 +35,12 @@ def get_page_langs(page_title, wiki_db):
         list_page_title_lang+=[r_out]
     return list_page_title_lang
 
-def get_page_inlinks(page_title, wiki_db):
+def get_page_inlinks(page_title, wiki_db, do_continue = True):
     """
     Get all inlinks for a given page (main namespace, non-redirects).
 
     Retrieves all inlinks in batches of 500 using continue https://www.mediawiki.org/wiki/API:Continue
+    if do_continue == False, we only get the first batch of 500 inlinks and wont use continue.
 
     """
     headers = {"User-Agent": "MGerlach_(WMF) WMF-Research"}
@@ -66,9 +67,74 @@ def get_page_inlinks(page_title, wiki_db):
         # if there is no continue then we got all results so we can stop
         if 'continue' not in response:
             break
+        if do_continue == False:
+            break
         # get the new continue parameter
         last_continue = response["continue"]
     return page_title_inlinks
+
+def get_page_outlinks(page_title, wiki_db):
+    """
+    Get all outlinks for a given page (main namespace).
+
+    Only returns first 500 results for now.
+
+    """
+    headers = {"User-Agent": "MGerlach_(WMF) WMF-Research"}
+    api_url_base = 'https://%s.wikipedia.org/w/api.php'%( wiki_db.replace('wiki','') )
+
+    params = {
+        'action': 'query',
+        'titles': page_title,
+        'prop': 'links',
+        # 'lhprop': 'title',
+        'plnamespace': '0',
+        # 'lhshow': '!redirect',
+        'pllimit': 500,
+        'format': 'json',
+    }
+    page_title_outlinks = []
+    last_continue = {}
+    while True:
+        params.update(last_continue)
+        response = requests.get( api_url_base,params=params, headers=headers).json()
+        results = list(response["query"]["pages"].values())[0].get("links",[])
+        for s in results:
+            page_title_outlinks += [s["title"].replace(" ","_")]
+        # if there is no continue then we got all results so we can stop
+        if 'continue' not in response:
+            break
+        # get the new continue parameter
+        last_continue = response["continue"]
+    # resolve redirects
+    page_title_outlinks_resolved = resolve_redirects_titles(page_title_outlinks,wiki_db)
+    return page_title_outlinks_resolved
+
+def resolve_redirects_titles(list_pages, wiki_db, n_batch = 50):
+    headers = {"User-Agent": "MGerlach_(WMF) WMF-Research"}
+    api_url_base = 'https://%s.wikipedia.org/w/api.php'%(  wiki_db.replace('wiki','') )
+
+    list_pages_resolved = []
+    if len(list_pages) > 0:
+        ## splitting into batches (n_batch=50)
+        list_pages_split = np.array_split(list_pages,math.ceil(len(list_pages)/n_batch))
+        for list_pages_batch in list_pages_split:
+            params = {
+                'action': 'query',
+                'titles': "|".join(list_pages_batch),
+                'redirects': "True",
+                'formatversion': "2",
+                'format': 'json',
+            }
+            try:
+                response = requests.get( api_url_base,params=params, headers=headers).json()
+                results = response["query"]["pages"]
+                for page in results:
+                    title = page["title"].replace(" ","_")
+                    list_pages_resolved += [title]
+            except:
+                pass
+    return sorted(list(set(list_pages_resolved)))
 
 def get_pages_lang(list_pages, wiki_db, wiki_db_translate, n_batch = 50):
     """
@@ -139,5 +205,42 @@ def link_translate_inlink(page_title, wiki_db, langs_translate=None):
     dict_out = {
         "inlinks_exist": page_title_inlinks,
         "inlinks_recs": list_links
+    }
+    return dict_out
+
+def link_translate_outlink(page_title, wiki_db, langs_translate=None):
+    list_links = []
+
+    # find the same article in other languages
+    page_title_langs = get_page_langs(page_title, wiki_db)
+    # find existing inlinks
+    page_title_outlinks = get_page_outlinks(page_title, wiki_db)
+    # find inlinks in each language and check if they exist in wiki_db
+    for r in page_title_langs:
+        p = r["page_title"]
+        w = r["wiki_db"]
+        # if we are only checking some languages
+        if langs_translate != None:
+            if isinstance(langs_translate,list):
+                if w.replace("wiki","") not in langs_translate:
+                    continue
+        r_outlinks = get_page_outlinks(p,w)
+        r_outlinks_translations = get_pages_lang(r_outlinks, w, wiki_db)
+        for r_t in r_outlinks_translations:
+            # remove if the translated inlink already exists
+            if r_t["page_title"].replace(" ","_") in page_title_outlinks:
+                continue
+            dict_out = {
+                "source":page_title,
+                "target":r_t["page_title"],
+                "source_translate": p,
+                "target_translate": r_t["page_title_original"],
+                "lang_translate": w.replace("wiki",""),
+            }
+            list_links += [dict_out]
+        # break
+    dict_out = {
+        "outlinks_exist": page_title_outlinks,
+        "outlinks_recs": list_links
     }
     return dict_out
